3c81f87d512afe301f4ae762d42f9405f14b52a6
meisponge <118172594+meisponge@users.noreply.github.com>
2022-11-21 18:57:41 +0900

    1

---
+++ いっかいめ.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+
+st.title("今日のごはんは何にしよう")
+st.write("あなたの体調から，今日の晩ごはんにおすすめのメニューを提案します")
+
+
+
+
+
