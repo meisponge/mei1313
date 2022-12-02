28b6c57e48e68808805fd1c25c9b338e4a4d86d0
meisponge <118172594+meisponge@users.noreply.github.com>
2022-12-02 15:41:24 +0900

    1202

--- photo.py
+++ photo.py
@@ -13,48 +13,48 @@ import random
 st.title("今日のごはんはなににしよう")
 st.write("体調を入力すると今日の晩ごはんにおすすめのメニューを表示します")
 
-menu=st.selectbox("どれを表示しますか",("選択無し","今日のごはん", "ごはんリスト"))
+menu=st.selectbox("どれを表示しますか",("選択なし","今日のごはん", "ごはんリスト","めいのおすすめ"))
 
 if menu=="ごはんリスト":
     col1,col2,col3 = st.columns(3)
     with col1:
        st.header("1")
-       st.image("1.JPG",caption='中華風おかゆ')
+       st.image("1.jpg",caption='中華風おかゆ')
     
     with col2:
        st.header("2")
-       st.image("2.JPG",caption='京風親子うどん')
+       st.image("2.jpg",caption='京風親子うどん')
        
     with col3:
        st.header("3")
-       st.image("3.JPG",caption='野菜と卵のぞうすい')
+       st.image("3.jpg",caption='野菜と卵のぞうすい')
     
     col4, col5,col6 = st.columns(3)
     with col4:
        st.header("4")
-       st.image("4.JPG",caption='カオマンガイ')
+       st.image("4.jpg",caption='カオマンガイ')
     
     with col5:
        st.header("5")
-       st.image("5.JPG",caption='親子丼')
+       st.image("5.jpg",caption='親子丼')
        
     with col6:
        st.header("6")
-       st.image("6.JPG",caption="野菜炒め")
+       st.image("6.jpg",caption="野菜炒め")
        
     col7, col8,col9 = st.columns(3) 
      
     with col7:
         st.header("7")
-        st.image("7.JPG",caption='カレー')
+        st.image("7.jpg",caption='カレー')
     
     with col8:
         st.header("8")
-        st.image("8.JPG",caption='お好み焼き')
+        st.image("8.jpg",caption='お好み焼き')
         
     with col9:
         st.header("9")
-        st.image("9.JPG",caption='からあげ')  
+        st.image("9.jpg",caption='からあげ')  
 
 elif menu=="今日のごはん":    
     condition1=st.slider("今日のだるさを教えてください（１…だるい，１０…だるくない）",1,5,10)
@@ -63,21 +63,29 @@ elif menu=="今日のごはん":
     
     condition3=st.slider("今日の顔色はどうですか（１…悪い，１０…良い）",1,5,10)
     
-    st.write(condition1 + condition2 + condition3)
     kekka = condition1 + condition2 + condition3
     
     if 0 <= kekka and kekka <= 10: 
         photonumber=random.randint(1,3)
-        st.image(str(photonumber)+".JPG")          
+        if st.button(label="表示ボタン",key=1):
+            st.image(str(photonumber)+".jpg")
+                  
     
     elif 11 <= kekka and kekka <= 20:
         photonumber=random.randint(4,6)
-        st.image(str(photonumber)+".JPG") 
+        if st.button(label="表示ボタン",key=2):
+            st.image(str(photonumber)+".jpg")
         
     else:
         photonumber=random.randint(7,9)
-        st.image(str(photonumber)+".JPG") 
+        if st.button(label="表示ボタン",key=3):
+            st.image(str(photonumber)+".jpg")
+                    
+elif menu=="選択なし":
+    st.write('')
+    
+elif menu=="めいのおすすめ":
+    st.image("kani.jpg")
 
-elif menu=="選択無し":
-    None
+    
     
